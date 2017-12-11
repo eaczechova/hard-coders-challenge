8a802dbde44e8e19e584977cba3eb544439e7fd9
Ewa Czech <eaprojekty@gmail.com>
2017-12-11 22:05:57 +0100

    solution with charAt()

--- reverse_a_string.js
+++ reverse_a_string.js
@@ -9,10 +9,22 @@
 // reverseString("hello");
 
 // 2. solution with string index
+
+// function reverseString(str) {
+//   var string = '';
+//   for (var i = str.length-1; i>=0; i--) {
+//     string += str[i];
+//   }
+//   return string;
+// }
+// reverseString("hello")
+
+// 3. solution with charAt()
+
 function reverseString(str) {
   var string = '';
-  for (var i = str.length-1; i>=0; i--) {
-    string += str[i];
+  for (var i = str.length; i>=0; i--) {
+    string += str.charAt(i);
   }
   return string;
 }

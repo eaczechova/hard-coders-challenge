4b173eac7651d65a21d0d85aad83735ea7ee4839
Ewa Czech <eaprojekty@gmail.com>
2017-12-13 15:16:29 +0100

    add solution with for loop

--- find_the_longest_word_in_a_string.js
+++ find_the_longest_word_in_a_string.js
@@ -1,3 +1,7 @@
+// Return the length of the longest word in the provided sentence.
+
+// 1. Solution with forEach() and reduce() methods:
+
 function findLongestWord(str) {
   var string = str.split(' ');
   string.forEach(function(element, index){
@@ -9,6 +13,21 @@ function findLongestWord(str) {
   return Math.max(a, b);
 });
   return max;
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
+
+// 2. Solution with for loop:
+
+function findLongestWord(str) {
+  var string = str.split(' ');
+  var stringLength = 0;
+  for(var i = 0; i<string.length; i++) {
+    if(string[i].length > stringLength) {
+      stringLength = string[i].length;
+    }
+  }
+  return stringLength;
 }
 
 findLongestWord("The quick brown fox jumped over the lazy dog");
